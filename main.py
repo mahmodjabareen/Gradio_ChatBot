a1e179011dc345e86b101e6135e3ac0f8f30f777
mahmod jabareen <mahmod.jabareen@cnvrg.io>
2023-09-12 15:34:02 +0300

    conversation tracking

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import requests
 bot_url="YOUR_URL"
 header_token="YOUR_TOKEN"
 model_uuid="YOUR_MODEL_UUID"
+messages = []
+
+
 #Sending the request
 def send_request(message, history):
+    # your message
+    new_message = {
+        "text": message,
+        "user_role": True
+    }
+    messages.append(new_message)
     request_payload = {
         "model_uuid": model_uuid,
         "data": {
-            "messages": [
-                {
-                    "text": message,
-                    "user_role": True
-                }
-            ],
+            "messages": messages,
             "inference_params": {
                 "max_new_tokens": 150,
                 "temperature": 0.5,
@@ -36,7 +40,12 @@ def send_request(message, history):
     reader = response.iter_content(chunk_size=128)
     for chunk in reader:
         chunk_response += chunk.decode('utf-8')
-
+        #appending the respond to the message
+    res = {
+        "text": chunk_response,
+        "user_role": False
+    }
+    messages.append(res)
     return chunk_response
 #Lunching the chat app
 chat_interface = gr.ChatInterface(fn=send_request)
